afad125b4a37f9e4c5a1421d45da62090cec45f3
Prashant G <prashant.gaikwad@leftrightmind.com>
2018-11-03 21:24:07 +0530

    Added Lottie files and source code

--- Lottie Animations/ViewController.swift
+++ Lottie Animations/ViewController.swift
@@ -7,14 +7,45 @@
 //
 
 import UIKit
+import Lottie
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var animationView1: LOTAnimatedControl!
+    @IBOutlet var animationView2: LOTAnimatedControl!
+    @IBOutlet var animationView3: LOTAnimatedControl!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        startAnimation1()
+        startAnimation2()
+        startAnimation3()
     }
 
+    func startAnimation1() {
+        let animationView = LOTAnimationView(name: "fireworks")
+        animationView.frame = animationView1.bounds
+        animationView.loopAnimation = true
+        animationView.play()
+        animationView1.animationView.addSubview(animationView)
+    }
+    
+    func startAnimation2() {
+        let animationView = LOTAnimationView(name: "moving_forward")
+        animationView.frame = animationView2.bounds
+        animationView.loopAnimation = true
+        animationView.play()
+        animationView2.animationView.addSubview(animationView)
+    }
+    
+    func startAnimation3() {
+        let animationView = LOTAnimationView(name: "infinite_rainbow")
+        animationView.frame = animationView3.bounds
+        animationView.loopAnimation = true
+        animationView.play()
+        animationView3.animationView.addSubview(animationView)
+    }
 
 }
 
